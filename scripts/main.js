b55115917232ecb959088e8b510ae079d878aebc
valkirijya <valkirijya@yandex.ua>
2020-02-26 19:47:27 +0200

    ghh

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+/*let btn = document.querySelector(".scroll-top-btn");
+
+btn.addEventListener('click', scrolling);
+
+const scrolling = () => {
+    document.body.scrollTop;
+} 
+
+console.log("worcs!")*/
