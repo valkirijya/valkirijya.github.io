178c46e72a11fb94e1fb0c014c4d1c088ac3f4d8
valkirijya <valkirijya@yandex.ua>
2020-02-26 23:36:43 +0200

    vf

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,18 @@
-/*let btn = document.querySelector(".scroll-top-btn");
+let btn = document.querySelector(".scroll-top-btn");
+console.log(btn);
 
-btn.addEventListener('click', scrolling);
+/*btn.addEventListener('click', scrolling);
+function scrolling () {
+    if(document.documentElement.clientHeight > window.pageYOffset) {
+        btn.setAttribute('style', "visibility: hidden;");   
+    }
+};*/
 
-const scrolling = () => {
-    document.body.scrollTop;
-} 
+window.addEventListener('scroll', check);
+function check() {
+if ((document.documentElement.clientHeight)/3 < window.pageYOffset) {
+    btn.setAttribute('style', "visibility: visible;");
+} else { btn.setAttribute('style', "visibility: hidden;");}
+};
 
-console.log("worcs!")*/
+console.log("worcs!");
