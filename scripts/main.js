bc7971cabe41dd394dc88093a2e5df3f51146be3
valkirijya <valkirijya@yandex.ua>
2020-02-27 18:28:16 +0200

    hbkh,

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,9 @@
 let btn = document.querySelector(".scroll-top-btn");
 console.log(btn);
 
-/*btn.addEventListener('click', scrolling);
-function scrolling () {
-    if(document.documentElement.clientHeight > window.pageYOffset) {
-        btn.setAttribute('style', "visibility: hidden;");   
-    }
-};*/
-
 window.addEventListener('scroll', check);
 function check() {
 if ((document.documentElement.clientHeight)/3 < window.pageYOffset) {
     btn.setAttribute('style', "visibility: visible;");
 } else { btn.setAttribute('style', "visibility: hidden;");}
 };
-
-console.log("worcs!");
