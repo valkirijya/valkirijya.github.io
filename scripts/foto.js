558fd19e8982c01645c694185b9ada8c277c8525
valkirijya <valkirijya@yandex.ua>
2020-03-04 22:31:45 +0200

    gnyty

--- scripts/foto.js
+++ scripts/foto.js
@@ -9,8 +9,37 @@ fetch(url)
       let element = document.createElement("img");
       element.className = "scale";
       element.setAttribute("src", `${outData[i]}`);
+      if (i % 2 == 0) {
+        element.setAttribute(
+          "style",
+          `transform: rotate(${Math.round(Math.random() * 10)}deg);`
+        );
+      } else {
+        element.setAttribute(
+          "style",
+          `transform: rotate(-${Math.round(Math.random() * 10)}deg);`
+        );
+      }
       let row = document.querySelector(".row");
       row.appendChild(element);
+    }
+    let img = document.querySelectorAll(".scale");
+    for (let i = 0; i < img.length; i++) {
+      img[i].addEventListener("mousedown", setOver);
+      function setOver() {
+        img[i].setAttribute(
+          "style",
+          "transform: scale(1.9); z-index: 20; padding: 8px; transition: 0.5s; "
+        );
+        img[i].addEventListener("mouseup", setOut);
+      function setOut() {
+        img[i].setAttribute(
+          "style",
+          `transform: rotate(${Math.round(Math.random() * 10)}deg); z-index: 1; transition: 0.2s; `
+        );
+      }
+      }
+     
     }
   })
   .catch(err => {
