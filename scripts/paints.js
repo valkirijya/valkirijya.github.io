aa73276a78077a639276e5f4b59e675bbd14a5d0
valkirijya <valkirijya@yandex.ua>
2020-03-13 15:50:55 +0200

    bfg

--- scripts/paints.js
+++ scripts/paints.js
@@ -1,6 +1,6 @@
 let butRight = document.querySelector(".right");
 let butLeft = document.querySelector(".left");
-
+let mainFoto = document.querySelector(".fild > div > img");
 let mas = [
   "images/paint/0.jfif",
   "images/paint/1.jfif",
@@ -13,11 +13,12 @@ let current = 0;
 butRight.addEventListener("click", stepRight);
 function stepRight() {
   let foto = document.querySelector(".fild > div > img");
-
   if (current < mas.length - 1) current++;
   else current = 0;
   foto.src = mas[current];
-}
+  foto.style.boxShadow = "unset";
+  foto.style.animation = "shadow 1s ease-in-out 0s 1 normal forwards";
+ }
 
 butLeft.addEventListener("click", stepLeft);
 function stepLeft() {
@@ -25,4 +26,6 @@ function stepLeft() {
   if (current > 0) current--;
   else current = mas.length - 1;
   foto.src = mas[current];
+  foto.style.animation = "shadow 2s ease-in-out 0s 1 normal forwards";
+  foto.style.animationPlayState = "running";
 }
